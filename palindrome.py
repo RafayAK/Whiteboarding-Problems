2a05a7282b3e8988f1e3a1834531cfdda485b849
LENYLANE <ALENA@KRUCREW.COM>
2016-04-11 10:19:06 -0700

    easy whiteboarding problem

---
+++ palindrome.py
@@ -0,0 +1,40 @@
+# Return True/False if this word is a palindrome.
+# A palindrome is a word that is spelled the same backwards and forwards.
+
+
+def is_palindrome(word):
+    """Return True if word is palindrome, else return False.
+
+        >>> is_palindrome("a")
+        True
+
+        >>> is_palindrome("noon")
+        True
+
+        >>> is_palindrome("racecar")
+        True
+
+        >>> is_palindrome("porcupine")
+        False
+
+        >>> is_palindrome("Racecar")
+        False
+
+    """
+
+    i = 0
+    j = -1
+
+    while i < len(word)/2:
+        if word[i] == word[j]:
+            i += 1
+            j -= 1
+        else:
+            return False
+    return True
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+
+# Runtime O(n)
---
+++ sum-list.py
@@ -0,0 +1,26 @@
+# Compute the sum of a list of numbers.
+
+
+def sum_list(num_list):
+    """Return the sum of all numbers in list.
+
+        >>> sum_list([5, 3, 6, 2, 1])
+        17
+
+        >>> sum_list([])
+        0
+
+    """
+
+    sum_of_nums = 0
+
+    for num in num_list:
+        sum_of_nums += num
+
+    return sum_of_nums
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+
+# Runtime O(n)
