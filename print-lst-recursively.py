eda3792e3253dde1364a0e3ba55b1ca3a8392b92
LENYLANE <ALENA@KRUCREW.COM>
2016-04-13 10:02:46 -0700

    medium whiteboarding problem

---
+++ print-lst-recursively.py
@@ -0,0 +1,22 @@
+# Print items from a list using recursion.
+
+
+def print_recursively(lst):
+    """Print items in the list, using recursion.
+
+        >>> print_recursively([1, 2, 3])
+        1
+        2
+        3
+
+    """
+
+    if lst:
+        print lst[0]
+        return print_recursively(lst[1:])
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+
+# Runtime O(1)
