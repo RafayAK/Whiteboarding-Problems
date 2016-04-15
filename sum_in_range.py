c3d7ba9747c965d79acff119c2b0c7481ec0c451
LENYLANE <ALENA@KRUCREW.COM>
2016-04-15 12:35:30 -0700

    easy  whiteboarding problem

---
+++ sum_in_range.py
@@ -0,0 +1,29 @@
+# Solution 1: Runtime O(n) Space complexity O(1)
+
+
+def sum_in_range(n):
+
+	sum_of_ints = 0
+	for i in range(n):
+		sum_of_ints += i
+
+return sum_of_ints
+
+
+# Sulution 2(Recursive): Runtime O(n) Space complexity O(n)  
+
+
+def sum_in_range(n):
+
+	if n <= o:
+		return 0
+	else:
+		return n + sum_in_range(n - 1)
+
+
+# Solution 3(Formula): Runtime O(1) Space complexity O(1)
+
+
+def sum_in_range(n):
+
+	sum_of_ints = ((n + 1) * n) / 2
